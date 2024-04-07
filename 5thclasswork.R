52246171e01c1cbdf3be38cb49f2c4601ecef27c
Pranav varma <you@example.com>
2024-04-06 21:50:35 -0400

    4/5/24 class work

---
+++ 5thclasswork.R
@@ -0,0 +1,34 @@
+#creating and 
+A <- 5
+B <- 10
+C <- A+B
+C
+V1 <- 4.5
+V2 <- 2
+V3 <- V1 / V2
+V3
+V4 <- sqrt(V1)
+V4
+greet <-"hello"
+name <-"BOb"
+message <-paste(greet,name)
+message
+# printed hello world
+"hello world@@"
+#integers
+x <-2L
+typeof(x)
+#double 
+y<- 2.5
+typeof(y)
+#complex
+z<- 2+3i
+typeof(z)
+result <- 4<5
+result
+typeof(result)
+result2 <-!(5>1)
+result2
+result | result2
+result & result2
+isTRUE(result)
